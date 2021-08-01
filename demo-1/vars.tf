4618f65e995640489575323c9b99592c22a7d9a7
Raghavendra Prasad <81212695+prasadg383@users.noreply.github.com>
2021-08-01 22:35:05 +0530

    Update vars.tf
    updated ami

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,19 +1,12 @@
-variable "AWS_ACCESS_KEY" {
-}
-
-variable "AWS_SECRET_KEY" {
-}
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "es-east-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    eu-west-1 = "ami-0c2b8ca1dad447f8a"
   }
 }
 
